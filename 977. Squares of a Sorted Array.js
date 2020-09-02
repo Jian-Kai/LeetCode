eb7a490131c035e2834c43c6f616c7848dcefeee
Jain-Kai,Kuo <tony14256@gmail.com>
2020-09-02 15:10:59 +0800

    Add files via upload

---
+++ 977. Squares of a Sorted Array.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} A
+ * @return {number[]}
+ */
+// https://leetcode.com/problems/squares-of-a-sorted-array/
+var sortedSquares = function (A) {
+  console.log(A);
+  const l = A.length;
+  const r = [];
+};
+
+// const input = [-4, -1, 0, 3, 10];
+// [0,1,9,16,100]
+
+const input = [-7, -3, 2, 3, 11];
+// Output: [4, 9, 9, 49, 121]
+
+sortedSquares(input);
